9c984f140cf8e5e24cb5f6fa2492d59c8e3da2f0
miracoly <68049792+miracoly@users.noreply.github.com>
2024-01-28 19:06:45 +0100

    ci: adapt nix shell script to build application

--- shell.nix
+++ shell.nix
@@ -1,7 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [
-        criterion
+    nativeBuildInputs = with pkgs.buildPackages;
+    let
+        ca65-symbls-to-nl = pkgs.stdenv.mkDerivation rec {
+            name = "ca65-symbls-to-nl";
+            version = "1.0";
+
+            src = ./.;
+
+            DESTDIR = "$(out)";
+        };
+    in [
+        ca65-symbls-to-nl
     ];
 }
